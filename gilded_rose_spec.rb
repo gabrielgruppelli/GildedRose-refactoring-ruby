8f42be2426d8fbfa096820a59756dd42f3bf24da
gabriel.gruppelli <gabriel.gruppelli@g.globo>
2021-05-11 16:01:50 -0300

    add tests for Aged Brie

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,10 +3,41 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    describe "Aged Brie" do
+      it "when sell in above 1 and quality bigger then 50" do
+        items = [Item.new("Aged Brie", 10, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 50
+      end
+
+      it "when sell in above 1 and quality bigger then 50" do
+        items = [Item.new("Aged Brie", 0, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 50
+      end
+
+      it "when sell is bigger then 0 and quality less then 50" do
+        items = [Item.new("Aged Brie", 10, 30)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 31
+      end
+
+      it "when sell is equal 0 and quality less then 50" do
+        items = [Item.new("Aged Brie", 0, 30)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 32
+      end
+
+      it "when sell is less 0 and quality less then 50" do
+        items = [Item.new("Aged Brie", -1, 32)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq -2
+        expect(items[0].quality).to eq 34
+      end
     end
   end
 

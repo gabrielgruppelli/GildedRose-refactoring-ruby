297953707da989817f630dc821824b937ff78ca1
Ana Carolina Castro <ana.castro@corp.globo.com>
2021-05-12 11:44:46 -0300

    Improve tests labels

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,111 +1,124 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-
   describe "#update_quality" do
-    describe "Aged Brie" do
-      it "when sell in is above 0 and quality bigger then 50" do
-        items = [Item.new("Aged Brie", 10, 50)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 9
-        expect(items[0].quality).to eq 50
-      end
+    context "Generic Item" do
+      context "when quality less then 50" do
+        it "decreases quality in 1 when sell in is above 0" do
+          items = [Item.new("Generic Item", 11, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 10
+          expect(items[0].quality).to eq 39
+        end
 
-      it "when sell in is above 0 and quality bigger then 50" do
-        items = [Item.new("Aged Brie", 0, 50)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -1
-        expect(items[0].quality).to eq 50
-      end
-
-      it "when sell in is above then 0 and quality less then 50" do
-        items = [Item.new("Aged Brie", 10, 30)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 9
-        expect(items[0].quality).to eq 31
-      end
-
-      it "when sell in is equal 0 and quality less then 50" do
-        items = [Item.new("Aged Brie", 0, 30)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -1
-        expect(items[0].quality).to eq 32
-      end
-
-      it "when sell in is less 0 and quality less then 50" do
-        items = [Item.new("Aged Brie", -1, 32)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -2
-        expect(items[0].quality).to eq 34
+        it "decreases quality in 2 when sell in is below 1" do
+          items = [Item.new("Generic Item", 0, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 38
+        end
       end
     end
 
-    describe "Backstage passes to a TAFKAL80ETC concert" do
-      it "when sell in is above 0 and quality less then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 40)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 10
-        expect(items[0].quality).to eq 41
+    context "Aged Brie" do
+      context "when quality is above 50" do
+        it "quality keeps the same if sell in is above 0" do
+          items = [Item.new("Aged Brie", 10, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+          expect(items[0].quality).to eq 50
+        end
+
+        it "quality keeps the same if sell in is below 0" do
+          items = [Item.new("Aged Brie", 0, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 50
+        end
       end
 
-      it "when sell in is above 0 and quality less then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 9
-        expect(items[0].quality).to eq 42
-      end
+      context "when quality below 50" do
+        it "quality increases in 1 if sell in is above 0" do
+          items = [Item.new("Aged Brie", 10, 30)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+          expect(items[0].quality).to eq 31
+        end
 
-      it "when sell in is above 0 and quality less then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 40)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 4
-        expect(items[0].quality).to eq 43
-      end
+        it "quality increases in 2 if sell in is equal 0" do
+          items = [Item.new("Aged Brie", 0, 30)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 32
+        end
 
-      it "when sell in is equal 0 and quality less then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -1
-        expect(items[0].quality).to eq 0
-      end
-
-      it "when sell in is above 0 and quality bigger then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 4
-        expect(items[0].quality).to eq 50
-      end
-
-      it "when sell in is equal 0 and quality bigger then 50" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -1
-        expect(items[0].quality).to eq 0
+        it "quality increases in 2 if sell in below 0" do
+          items = [Item.new("Aged Brie", -1, 32)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -2
+          expect(items[0].quality).to eq 34
+        end
       end
     end
 
-    describe "Sulfuras, Hand of Ragnaros" do
-      it "Don't change" do
-        items = [Item.new("Sulfuras, Hand of Ragnaros", 11, 40)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 11
-        expect(items[0].quality).to eq 40
+    context "Backstage passes to a TAFKAL80ETC concert" do
+      context "when quality is less then 50" do
+        it "quality increases 1 if sell in is above 10" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 10
+          expect(items[0].quality).to eq 41
+        end
+
+        it "quality increases 2 if sell in is below 10 and above 5" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+          expect(items[0].quality).to eq 42
+        end
+
+        it "quality increases 3 if sell in is below 5 and above 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 4
+          expect(items[0].quality).to eq 43
+        end
+
+        it "quality becomes 0 if sell in is equal 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 0
+        end
+      end
+
+      context "when quality bigger then 50"  do
+        it "quality keeps 50 when sell in is above 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 4
+          expect(items[0].quality).to eq 50
+        end
+
+        it "quality becomes 0 if sell in is equal 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq -1
+          expect(items[0].quality).to eq 0
+        end
       end
     end
 
-    describe "Generic Item" do
-      it "when sell in is above 0 and quality less then 50" do
-        items = [Item.new("Generic Item", 11, 40)]
+    context "Sulfuras, Hand of Ragnaros" do
+      it "always stays the same" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
         GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 10
-        expect(items[0].quality).to eq 39
-      end
+        expect(items[0].sell_in).to eq 0
+        expect(items[0].quality).to eq 80
 
-      it "when sell in is equal 0 and quality less then 50" do
-        items = [Item.new("Generic Item", 0, 40)]
         GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq -1
-        expect(items[0].quality).to eq 38
+        expect(items[0].sell_in).to eq 0
+        expect(items[0].quality).to eq 80
       end
     end
   end
